4ff3283c06d06285d64f096194d881de5b1d2a97
alighorbani <alighorbani20002@gmail.com>
2021-02-06 09:18:25 +0330

    fixed config file

---
+++ config/headers.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    
+    'Accept' => "application/json, text/javascript, */*; q=0.01",
+
+    'Accept-Encoding' => "gzip, deflate",
+
+    'Accept-Language' => "en-US,en;q=0.5",
+
+    'Cache-Control' => "no-cache",
+
+    'Connection' => "keep-alive",
+
+    'Content-Length' => "different-value",
+
+    'Content-Type' => "application/json",
+
+    'Cookie' => "JSESSIONID=AAB8A91375AE23161057228BF8D38D87",
+
+    'Host' => "hsmai.ir",
+
+    'Origin' => "http://hsmai.ir",  
+
+    'Pragma' => "no-cache'",
+
+    'Referer' => "http://hsmai.ir/newpanels/'",
+
+    'User-Agent' => "different-value",
+
+    'X-Requested-With' => "XMLHttpRequest",
+];
+
+// 'Cookie' => "JSESSIONID=D0DE1F15EAA430364802E36975174A87; questionPoolQuestionListquestionPoolQuestionListRepeater={%22filter%22:{%22text%22:%22All%22%2C%22value%22:%22all%22}%2C%22view%22:%22list%22%2C%22pageSize%22:10%2C%22pageIndex%22:0}; studentOnlineExamineListstudentOnlineExaminesRepeater={%22filter%22:{%22text%22:%22%D9%86%DB%8C%D9%85%D8%B3%D8%A7%D9%84%20%D8%A7%D9%88%D9%84%2099-1400%22%2C%22selected%22:true%2C%22value%22:7}%2C%22view%22:%22list%22%2C%22pageSize%22:10%2C%22pageIndex%22:0}; sidebarCollapsed=false; hfw_SID=; studentOMRExamineListstudentOMRExaminesRepeater={%22filter%22:{%22text%22:%22%D9%86%DB%8C%D9%85%D8%B3%D8%A7%D9%84%20%D8%A7%D9%88%D9%84%2099-1400%22%2C%22selected%22:true%2C%22value%22:7}%2C%22view%22:%22list%22%2C%22pageSize%22:10%2C%22pageIndex%22:0}",
